00b9b9d01c8c9f8208a9ba79813a58d43b1c5215
ericwang13 <ericwang13@protonmail.com>
2021-01-13 10:52:12 -0500

    Implementde and tested nth() and length()

--- Radix.java
+++ Radix.java
@@ -1,10 +1,14 @@
 public class Radix {
     public static int nth(int n, int col) {
-        return 0;
+        if (col > length(n) - 1) {
+            throw new IllegalArgumentException("Column " + col + " is out of range");
+        }
+
+        return (int) (n / Math.pow(10, col)) % 10;
     }
 
     public static int length(int n) {
-        return 0;
+        return (int) Math.log10(n) + 1;
     }
 
     public static void merge(MyLinkedList original, MyLinkedList[] buckets) {
