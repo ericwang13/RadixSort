4a713c2539f3d1b3d30766c64f0728140e28172b
ericwang13 <ericwang13@protonmail.com>
2021-01-13 12:13:13 -0500

    Completed radixSortSimple

--- Radix.java
+++ Radix.java
@@ -1,8 +1,8 @@
 public class Radix {
     public static int nth(int n, int col) {
-        if (col > length(n) - 1) {
-            throw new IllegalArgumentException("Column " + col + " is out of range");
-        }
+        // if (col > length(n) - 1) {
+        // throw new IllegalArgumentException("Column " + col + " is out of range");
+        // }
 
         return (int) (n / Math.pow(10, col)) % 10;
     }
@@ -17,8 +17,31 @@ public class Radix {
         }
     }
 
-    public static void radixSortSimple(SortableLinkedList data) {
+    public static void merge(SortableLinkedList original, SortableLinkedList[] buckets) {
+        for (SortableLinkedList i : buckets) {
+            original.extend(i);
+        }
+    }
 
+    public static void radixSortSimple(SortableLinkedList data) {
+        SortableLinkedList[] buckets = new SortableLinkedList[10];
+        for (int i = 0; i < buckets.length; ++i) {
+            buckets[i] = new SortableLinkedList();
+        }
+
+        int length = 1;
+        for (int i = 0; i < length; i++) {
+            int curr;
+            while (data.size() > 0) {
+                curr = data.remove(0);
+                if (i == 0) {
+                    length = Math.max(length, curr);
+                }
+
+                buckets[nth(curr, i)].add(curr);
+            }
+            merge(data, buckets);
+        }
     }
 
     public static void radixSort(SortableLinkedList data) {
